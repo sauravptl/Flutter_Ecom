3db327dc7afca42f951f33c9a55b5dc144fc3a23
saurav.ptl <saurav123patel@gamil.com>
2021-08-20 11:37:08 +0530

    all most done

--- ecom/lib/main.dart
+++ ecom/lib/main.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/material.dart';
-import 'screens/login.dart';
-import 'screens/cart.dart';
-import 'screens/home.dart';
+import 'screens/shop_list.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'ShopX',
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Login(),
-        '/home': (context) => Home(),
-        '/cart': (context) => Cart(),
-      },
+      home: ShopListWidget(),
     );
   }
 }
