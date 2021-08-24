fd1a2672a72a5011b367a4ae473a69e47bb00e14
saurav.ptl <saurav123patel@gamil.com>
2021-08-24 17:16:59 +0530

    product page

--- ecom/lib/screens/productDetails.dart
+++ ecom/lib/screens/productDetails.dart
@@ -32,77 +32,92 @@ class _ProductScreenState extends State<ProductScreen> {
           ),
         ],
       ),
-      body: GridView.builder(
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.blue[100],
-            ),
-            child: GridTile(
-              child: GestureDetector(
-                child: Center(
-                  child: Text('Product Image'),
-                  //child: Image.asset(productData.imageUrl,fit: BoxFit.cover,),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: GridView.builder(
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+                border: Border.all(
+                  color: Colors.blueGrey,
+                  width: 2,
                 ),
-                onTap: () {
-                  Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                      arguments: productData.items[index].id);
-                },
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12)),
               ),
-              footer: GridTileBar(
-                backgroundColor: Colors.blueGrey,
-                title: Text(
-                  productData.items[index].title,
-                  textAlign: TextAlign.center,
-                ),
-                trailing: IconButton(
-                  onPressed: () {
-                    if (valid) {
-                      cart.addItem(
-                          productData.items[index].id,
-                          productData.items[index].price,
-                          productData.items[index].title);
-                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text('Added to cart, Check cart'),
-                          duration: Duration(seconds: 3),
-                        ),
-                      );
-                    } else {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: const Text('Please Login First'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text('Cancel'),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pushNamedAndRemoveUntil(
-                                    '/', (route) => false);
-                              },
-                              child: const Text('OK'),
-                            )
-                          ],
-                        ),
-                      );
-                    }
+              child: GridTile(
+                child: GestureDetector(
+                  child: Center(
+                    child: Text(
+                      'Product Image',
+                      style: TextStyle(
+                          color: Colors.blueGrey, fontWeight: FontWeight.bold),
+                    ),
+                    //child: Image.asset(productData.imageUrl,fit: BoxFit.cover,),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushNamed(
+                        ProductDetailScreen.routeName,
+                        arguments: productData.items[index].id);
                   },
-                  icon: const Icon(Icons.shopping_cart),
+                ),
+                footer: GridTileBar(
+                  backgroundColor: Colors.blueGrey,
+                  title: Text(
+                    productData.items[index].title,
+                    textAlign: TextAlign.center,
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {
+                      if (valid) {
+                        cart.addItem(
+                            productData.items[index].id,
+                            productData.items[index].price,
+                            productData.items[index].title);
+                        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Added to cart, Check cart'),
+                            duration: Duration(seconds: 3),
+                          ),
+                        );
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: const Text('Please Login First'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pushNamedAndRemoveUntil(
+                                      '/', (route) => false);
+                                },
+                                child: const Text('OK'),
+                              )
+                            ],
+                          ),
+                        );
+                      }
+                    },
+                    icon: const Icon(Icons.shopping_cart),
+                  ),
                 ),
               ),
             ),
           ),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          itemCount: productData.items.length,
         ),
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemCount: productData.items.length,
       ),
     );
   }
